041d8279004907aa54f92d9101bed30cbd298d32
Daniel Lin <danielly@tw.ibm.com>
2021-08-04 10:45:53 +0800

    first commit

---
+++ src/main/java/com/ibm/demo/HelloWorldSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSampleApplication.class, args);
+	}
+
+}
